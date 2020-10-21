03f4fc92a5213337a38434df4a5ec278d60d0a8f
freddan88 <fredrik@leemann.se>
2020-10-21 11:05:24 +0200

    Initialize npm and add QLab string-algorithm challenge

---
+++ string.js
@@ -0,0 +1,24 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const removeIdenticalInstances = (string) => {
+  [...string].forEach((char) => {
+    let regex = new RegExp(`${char}{4}`, "g");
+    if (regex.test(string)) {
+      string = string.replace(regex, char + char + char);
+    }
+  });
+  return string;
+};
+
+rl.on("line", (line) => {
+  const qlres = removeIdenticalInstances(line);
+  console.log("");
+  console.log(`_Input: ${line}`);
+  console.log(`Output: ${qlres}`);
+  console.log("");
+});
